128a890a03f3bc63838d899d3e4e5fdca3296e8c
anik558363 <anikmondol558363@gmil.com>
2024-05-09 21:57:02 +0600

    set layout and make react responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
-  
+
 
   return (
     <>
-      
+
       <Header></Header>
-     <Blogs></Blogs>
+      <div className='md:flex items-center'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
