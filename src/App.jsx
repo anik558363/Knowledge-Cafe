68f4bbfa5af7bcd818e31c5ef63f730744ff1516
anik558363 <anikmondol558363@gmil.com>
2024-05-09 20:48:00 +0600

    create fake data for the blogs and load data in the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
   
@@ -7,9 +9,8 @@ function App() {
   return (
     <>
       
-      <h1 className='text-6xl'>Knowledge-Cafe</h1>
-      
-     
+      <Header></Header>
+     <Blogs></Blogs>
     </>
   )
 }
